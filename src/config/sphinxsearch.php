337f5389646b9e2888f5872bfe7d0e5109788ef1
DariusIII <dkrisan@gmail.com>
2018-02-05 15:45:58 +0100

    Update config file and readme

--- src/config/sphinxsearch.php
+++ src/config/sphinxsearch.php
@@ -4,7 +4,10 @@ return [
     'port'    => 9312,
     'timeout' => 30,
     'indexes' => [
-        'my_index_name' => ['table' => 'keywords', 'column' => 'id'],
+        'my_index_name' => [
+        	'table' => 'keywords',
+	        'column' => 'id'
+        ],
     ],
     'mysql_server' => [
         'host' => '127.0.0.1',
